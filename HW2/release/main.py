7e985b303bf8038365684b59c0da0edb4d9fa4b9
Nightsky <38932681+nightsky37@users.noreply.github.com>
2024-04-24 23:05:22 +0800

    Update main.py final

--- HW2/release/main.py
+++ HW2/release/main.py
@@ -5,7 +5,7 @@ import numpy.typing as npt
 import pandas as pd
 from loguru import logger
 from sklearn import metrics
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 
 
 class LogisticRegression:
@@ -23,10 +23,10 @@ class LogisticRegression:
         gradient = np.dot(x.T, (y_pred - y)) / m
         return cost[0], gradient
 
-    def fit(self, 
-        inputs: npt.NDArray[np.float_], 
-        targets: t.Sequence[int],
-    ) -> None:
+    def fit(self,
+            inputs: npt.NDArray[np.float_],
+            targets: t.Sequence[int],
+            ) -> None:
         rows = inputs.shape[0]
         targets = targets.reshape(rows, 1)
         costs = []
@@ -39,18 +39,19 @@ class LogisticRegression:
             self.weights -= self.learning_rate * gradient
             costs.append(cost)
 
-        self.intercept = float(self.weights[-1])
+        self.intercept = self.weights[len(self.weights) - 1]
+        self.intercept = float(self.intercept[0])
         self.weights = self.weights[:-1]
 
     def predict(
         self,
         inputs: npt.NDArray[np.float_],
-    ) -> t.Tuple[t.Sequence[np.float_], t.Sequence[int]]:      
+    ) -> t.Tuple[t.Sequence[np.float_], t.Sequence[int]]:
         inputs = np.append(inputs, np.ones([len(inputs), 1]), 1)
         self.weights = np.append(self.weights, self.intercept)
         pred = self.sigmoid(np.dot(inputs, self.weights))
         pred_classes = np.zeros(len(pred))
-        pred_classes[np.where(pred>0.5)] = 1
+        pred_classes[np.where(pred > 0.5)] = 1
 
         return pred, pred_classes
 
@@ -80,23 +81,20 @@ class FLD:
         self.m1 = np.mean(X1, axis=0)
         s0 = np.cov(X0, rowvar=False)
         s1 = np.cov(X1, rowvar=False)
-        self.sw = s0+s1
-        self.sb = np.dot((self.m0-self.m1).reshape(-1, 1), (self.m0-self.m1).reshape(-1, 1).T)
+        self.sw = s0 + s1
+        self.sb = np.dot((self.m0 - self.m1).reshape(-1, 1), (self.m0 - self.m1).reshape(-1, 1).T)
         invSW_by_SB = np.dot(np.linalg.inv(self.sw), self.sb)
-        
+
         eigenvalues, eigenvectors = np.linalg.eig(invSW_by_SB)
-        self.w = eigenvectors[:, np.argmax(eigenvalues)].reshape(-1, 1)*-1
+        self.w = eigenvectors[:, np.argmax(eigenvalues)].reshape(-1, 1) * -1
         y0 = np.dot(X0, self.w)
         y1 = np.dot(X1, self.w)
         y0m = np.mean(y0)
         y1m = np.mean(y1)
-        self.thresh = (y0m+y1m)/2
-        self.slope = self.w[1] / self.w[0]
-        print("slope:", self.slope)
-        b = -0.5 * (self.m0 @ np.linalg.inv(self.sw) @ self.m0.T) + 0.5 * (self.m1 @ np.linalg.inv(self.sw) @ self.m1.T)
-        print("b:", b)
-        self.intercept = -b/self.w[1]
-        print("intercept:", -b/self.w[1])
+        self.thresh = (y0m + y1m) / 2
+        self.slope = self.w[1][0] / self.w[0][0]
+        self.slope = float(self.slope)
+        # print("slope:", self.slope)
 
     def predict(
         self,
@@ -105,39 +103,36 @@ class FLD:
         y_p = np.dot(inputs, self.w)
         y_pred = np.zeros(len(y_p))
         y_pred = y_pred.reshape((len(y_pred), 1))
-        y_pred[y_p>=self.thresh] = 1
+        y_pred[y_p >= self.thresh] = 1
         y_pred = y_pred.flatten()
 
         return y_pred
 
     def plot_projection(self, inputs: npt.NDArray[np.float_]):
         y_pred = self.predict(inputs)
-        x0 = inputs[y_pred==0]
-        # x0 = x0.reshape(len(x0), 1)
-        x1 = inputs[y_pred==1]
-        print("x0:", x0.shape)   
-        print("x1:", x1.shape)
-        plt.scatter(x0[:, 0], x0[:, 1] ,color='red')
-        plt.scatter(x1[:, 0], x1[:, 1], color='blue')
-        y0 = np.dot(x0, self.w)
-        y1 = np.dot(x1, self.w)
-        print("x00:", x0[:, 0].reshape(-1,1).shape)
-        print("add:", np.add(x0[:, 0].reshape(-1,1),y0/self.slope).shape)
-        plt.scatter(self.slope, y0, color='red')
-        plt.scatter(y1/self.slope, y1, color='blue')
-        # for i in range(inputs.shape[0]):
-        #     plt.plot()
+        x0 = inputs[y_pred == 0]
+        x1 = inputs[y_pred == 1]
+        plt.scatter(x0[:, 0], x0[:, 1], color='red', s=10)
+        plt.scatter(x1[:, 0], x1[:, 1], color='blue', s=10)
 
-        x = [np.min(inputs[:, 0]), np.max(inputs[:, 0])]
-        y = [np.min(inputs[:, 1]), np.max(inputs[:, 1])]
-        x_lin = np.linspace(-1, 1, 50)
+        for i in range(x0.shape[0]):
+            project_point = np.dot(x0[i], self.w) * self.w  # pp = project and multiply dir vector
+            plt.scatter(project_point[0][0], project_point[1][0], color='red', s=15)
+            plt.plot([project_point[0][0], x0[i][0]], [project_point[1][0], x0[i][1]], color='gray', linewidth=1)
+
+        for i in range(x1.shape[0]):
+            project_point = np.dot(x1[i], self.w) * self.w  # pp = project and multiply dir vector
+            plt.scatter(project_point[0][0], project_point[1][0], color='blue', s=15)
+            plt.plot([project_point[0][0], x1[i][0]], [project_point[1][0], x1[i][1]], color='grey', linewidth=1)
+
+        x_lin = np.linspace(min(min(inputs[:, 0]), min(inputs[:, 1])), max(max(inputs[:, 0]), max(inputs[:, 1])), 100)
         projection_line = self.slope * x_lin
-        print("x:", x)
-        plt.plot(x_lin , projection_line)
-        # plt.xlim(-1,1)
-        # plt.ylim(-1, 1)
+        plt.plot(x_lin, projection_line)
+        plt.title("Projection Line: w=%.6f, b=0" % self.slope)
+
         plt.axis('square')
         plt.show()
+
 
 def compute_auc(y_trues, y_preds) -> float:
     return metrics.roc_auc_score(y_trues, y_preds)
@@ -162,15 +157,15 @@ def main():
     x_test = test_df.drop(['target'], axis=1).to_numpy()
     y_test = test_df['target'].to_numpy()
 
-    LR = LogisticRegression( 
-        learning_rate = 0.0005,  # You can modify the parameters as you want
-        num_iterations = 1000,  # You can modify the parameters as you want
+    LR = LogisticRegression(
+        learning_rate=0.0005,  # You can modify the parameters as you want
+        num_iterations=13000,  # You can modify the parameters as you want
     )
     LR.fit(x_train, y_train)
     y_pred_probs, y_pred_classes = LR.predict(x_test)
     accuracy = accuracy_score(y_test, y_pred_classes)
     auc_score = compute_auc(y_test, y_pred_probs)
-    logger.info(f'LR: Weights: {LR.weights[:5]}, Intercep: {LR.intercept}')
+    logger.info(f'LR: Weights: {LR.weights[:5]}, Intercept: {LR.intercept}')
     logger.info(f'LR: Accuracy={accuracy:.4f}, AUC={auc_score:.4f}')
 
     # Part2: FLD
