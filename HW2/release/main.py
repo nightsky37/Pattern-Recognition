5d9dcba12006e011db26bc4c82bdfd6808004431
Nightsky <38932681+nightsky37@users.noreply.github.com>
2024-04-24 17:16:27 +0800

    Update main.py

--- HW2/release/main.py
+++ HW2/release/main.py
@@ -20,7 +20,6 @@ class LogisticRegression:
         y_pred = self.sigmoid(np.dot(x, self.weights))
         error = (y * np.log(y_pred)) + ((1 - y) * np.log(1 - y_pred))
         cost = -1 / m * sum(error)
-        # print("cost: ", cost)
         gradient = np.dot(x.T, (y_pred - y)) / m
         return cost[0], gradient
 
@@ -30,40 +29,28 @@ class LogisticRegression:
     ) -> None:
         rows = inputs.shape[0]
         targets = targets.reshape(rows, 1)
-        # print("targets:", targets.shape)
         costs = []
         inputs = np.append(inputs, np.ones([len(inputs), 1]), 1)
-        print("inputs:", inputs.shape)
         self.weights = np.zeros(inputs.shape[1])
         self.weights = self.weights.reshape(self.weights.shape[0], 1)
-        print("weights:", self.weights.shape)
 
         for i in range(self.num_iterations):
             cost, gradient = self.compute_cost(inputs, targets)
             self.weights -= self.learning_rate * gradient
             costs.append(cost)
 
-        self.intercept = self.weights[-1]
+        self.intercept = float(self.weights[-1])
         self.weights = self.weights[:-1]
 
     def predict(
         self,
         inputs: npt.NDArray[np.float_],
     ) -> t.Tuple[t.Sequence[np.float_], t.Sequence[int]]:      
-        """
-        Implement your prediction function here.
-        The return should contains
-        1. sample probabilty of being class_1
-        2. sample predicted class
-        """
         inputs = np.append(inputs, np.ones([len(inputs), 1]), 1)
         self.weights = np.append(self.weights, self.intercept)
         pred = self.sigmoid(np.dot(inputs, self.weights))
         pred_classes = np.zeros(len(pred))
         pred_classes[np.where(pred>0.5)] = 1
-        # print("pred: ", pred)
-        # print(">=0.5: ", np.where(pred>=0.5))
-        # print("pred_classes: ", pred_classes)
 
         return pred, pred_classes
 
@@ -79,7 +66,8 @@ class FLD:
         self.sw = None
         self.sb = None
         self.slope = None
-        self.thres = None
+        self.thresh = None
+        self.intercept = None
 
     def fit(
         self,
@@ -88,65 +76,68 @@ class FLD:
     ) -> None:
         X0 = inputs[targets == 0]
         X1 = inputs[targets == 1]
-        print("X0:", X0.shape)
-        print("X1:", X1.shape)
         self.m0 = np.mean(X0, axis=0)
-        self.m0 = self.m0
         self.m1 = np.mean(X1, axis=0)
-        self.m1 = self.m1
-        print("m0:", self.m0.shape)
-        print("m1-m0:", (self.m1-self.m0).shape)
-        # s0 = np.dot((X0-self.m0).T, (X0-self.m0))
-        # s1 = np.dot((X1-self.m1).T, (X1-self.m1))
-# －0.6275043821234734088800495144518
-# －0.6275044049684719859494350420153
         s0 = np.cov(X0, rowvar=False)
         s1 = np.cov(X1, rowvar=False)
-        print("s0:", s0.shape)
-        print("s1:", s1.shape)
-        m = np.mean(inputs, axis=0)
-        print("m:", m.shape)
         self.sw = s0+s1
-        self.sb = np.dot((self.m1-self.m0).reshape(-1, 1), (self.m1-self.m0).reshape(-1, 1).T) # 直接平方?
-        # self.sb = (self.m1-self.m0).reshape(-1, 1)**2
-        invSW_by_SB = np.linalg.inv(self.sw) @ self.sb
-        print("invSWSB:", invSW_by_SB.shape)
-        # self.w = invSW_by_SB.reshape(-1,1)
+        self.sb = np.dot((self.m0-self.m1).reshape(-1, 1), (self.m0-self.m1).reshape(-1, 1).T)
+        invSW_by_SB = np.dot(np.linalg.inv(self.sw), self.sb)
         
         eigenvalues, eigenvectors = np.linalg.eig(invSW_by_SB)
-        print("eigvec:", eigenvectors)
-        print("eigval:", eigenvalues)
-        self.w = eigenvectors[:, 1].reshape(-1, 1)*-1
-        print("w:", self.w.shape)
-        # self.slope = eigenvalues
-
+        self.w = eigenvectors[:, np.argmax(eigenvalues)].reshape(-1, 1)*-1
         y0 = np.dot(X0, self.w)
         y1 = np.dot(X1, self.w)
         y0m = np.mean(y0)
         y1m = np.mean(y1)
-        self.thres = (y0m+y1m)/2
+        self.thresh = (y0m+y1m)/2
+        self.slope = self.w[1] / self.w[0]
+        print("slope:", self.slope)
+        b = -0.5 * (self.m0 @ np.linalg.inv(self.sw) @ self.m0.T) + 0.5 * (self.m1 @ np.linalg.inv(self.sw) @ self.m1.T)
+        print("b:", b)
+        self.intercept = -b/self.w[1]
+        print("intercept:", -b/self.w[1])
 
     def predict(
         self,
         inputs: npt.NDArray[np.float_],
     ) -> t.Sequence[t.Union[int, bool]]:
         y_p = np.dot(inputs, self.w)
-        m = float(np.mean(y_p))
-        print("y_p:", y_p.shape)
-        print("y_pm:", self.thres)
         y_pred = np.zeros(len(y_p))
-        print("y_pred:", y_pred.shape)
         y_pred = y_pred.reshape((len(y_pred), 1))
-        y_pred[y_p>=self.thres] = 1
+        y_pred[y_p>=self.thresh] = 1
         y_pred = y_pred.flatten()
 
         return y_pred
 
     def plot_projection(self, inputs: npt.NDArray[np.float_]):
         y_pred = self.predict(inputs)
-        y0 = np.zeros(len(y_pred))
-        y1 = np.zeros(len(y_pred))
-        y0[np.where(y_pred<0)] = 0
+        x0 = inputs[y_pred==0]
+        # x0 = x0.reshape(len(x0), 1)
+        x1 = inputs[y_pred==1]
+        print("x0:", x0.shape)   
+        print("x1:", x1.shape)
+        plt.scatter(x0[:, 0], x0[:, 1] ,color='red')
+        plt.scatter(x1[:, 0], x1[:, 1], color='blue')
+        y0 = np.dot(x0, self.w)
+        y1 = np.dot(x1, self.w)
+        print("x00:", x0[:, 0].reshape(-1,1).shape)
+        print("add:", np.add(x0[:, 0].reshape(-1,1),y0/self.slope).shape)
+        plt.scatter(self.slope, y0, color='red')
+        plt.scatter(y1/self.slope, y1, color='blue')
+        # for i in range(inputs.shape[0]):
+        #     plt.plot()
+
+        x = [np.min(inputs[:, 0]), np.max(inputs[:, 0])]
+        y = [np.min(inputs[:, 1]), np.max(inputs[:, 1])]
+        x_lin = np.linspace(-1, 1, 50)
+        projection_line = self.slope * x_lin
+        print("x:", x)
+        plt.plot(x_lin , projection_line)
+        # plt.xlim(-1,1)
+        # plt.ylim(-1, 1)
+        plt.axis('square')
+        plt.show()
 
 def compute_auc(y_trues, y_preds) -> float:
     return metrics.roc_auc_score(y_trues, y_preds)
@@ -154,7 +145,6 @@ def compute_auc(y_trues, y_preds) -> float:
 
 def accuracy_score(y_trues, y_preds) -> float:
     y_len = len(y_trues)
-    print("y_preds: ", y_preds)
     sumTP = np.sum(y_trues == y_preds.astype(int))
     return sumTP / y_len
 
@@ -172,7 +162,7 @@ def main():
     x_test = test_df.drop(['target'], axis=1).to_numpy()
     y_test = test_df['target'].to_numpy()
 
-    LR = LogisticRegression( #13000
+    LR = LogisticRegression( 
         learning_rate = 0.0005,  # You can modify the parameters as you want
         num_iterations = 1000,  # You can modify the parameters as you want
     )
